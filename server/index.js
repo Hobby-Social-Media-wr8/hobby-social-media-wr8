cd9a039cf11c268aaa6cfcaff206dcd62ee138e2
juancedeno96 <cedenojj7@gmail.com>
2021-02-19 15:41:53 -0700

    set up boilerplate code for server/index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const massive = require("massive");
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const session = require("express-session"),
+express = require('express');
+
+const app = express();
+app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false },
+  }).then((db) => {
+    app.set("db", db);
+    console.log("db connected");
+    app.listen(SERVER_PORT, () => console.log(`running on port ${SERVER_PORT}`));
+  });
+
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: SESSION_SECRET,
+      cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 },
+    })
+  );
+
+
