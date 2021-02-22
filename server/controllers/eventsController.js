a72682a00a25ef37d27fd95cb88741d5af8520b4
janellebaguley <nellie.baguley@gmail.com>
2021-02-22 08:40:34 -0700

    updates to events

--- server/controllers/eventsController.js
+++ server/controllers/eventsController.js
@@ -1,3 +1,11 @@
 module.export = {
+    getEvents: async(req, res) => {
+        const  {search} = req.query
+        const db = req.app.get('db')
+
+        await db.events.search_events([`%${search.toLowerCase()}%`])
+        .then(orderItems => res.status(200).send(orderItems))
+        .catch(err => res.status(500).send(err))
+    }
     
 }
