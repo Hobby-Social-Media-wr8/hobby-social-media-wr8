cefa287fbeacc25df6c571ae2759915070a522c3
MichalinDennie <miichalin17@gmail.com>
2021-03-03 18:02:30 -0600

    no changes, just referencing another component

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,9 @@ import Blog from "./Components/Blog/Blog";
 import Home from "./Components/Home/Home";
 import Chat from "./Components/Chat/Chat";
 import Message from "./Components/Chat/Message";
-<<<<<<< HEAD
-import Events from "./Components/Events/Events";
-import AddPost from './Components/Blog/AddPost'
-// import GroupsPage from "./Components/Groups/GroupsPage";
-=======
 import MyCalendar from "./Components/Calendar/MyCalendar";
 import AddPost from './Components/Blog/AddPost'
 // import Group from "./Components/Group/GroupPage";
->>>>>>> main
 import Profile from "./Components/Profile/Profile";
 import GroupsPage from "./Components/Groups/GroupsPage"
 
@@ -26,11 +20,7 @@ export default (
     <Route path="/events" component={MyCalendar} />
     <Route path="/chat/:id" component={Chat} />
     <Route path="/message" component={Message} />
-<<<<<<< HEAD
     <Route path="/groups" component={GroupsPage} />
-=======
-    {/* <Route path="/groups" component={Group} /> */}
->>>>>>> main
     <Route path="/profile" component={Profile} />
   </Switch>
 );
